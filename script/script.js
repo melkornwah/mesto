a0fccb42e1c15586056e0e939435d7c3d4395296
Vladimir Chisty <v.chistyi@gmail.com>
2020-11-05 23:35:15 +0300

    feat: add second form, rewrite script.js for multiple forms

--- script/script.js
+++ script/script.js
@@ -1,35 +1,63 @@
-const formElement = document.querySelector(".form");
+const formProfile = document.querySelector(".form-profile");
+const formPlace = document.querySelector(".form-place");
 
-const profileElement = document.querySelector(".profile__info");
-const profileName = profileElement.querySelector(".profile__name");
-const profileJob = profileElement.querySelector(".profile__desc");
+const profileInfo = document.querySelector(".profile__info");
+const profileName = profileInfo.querySelector(".profile__name");
+const profileJob = profileInfo.querySelector(".profile__desc");
 
-const closeButton = document.querySelector(".button_action_close");
-const editButton = document.querySelector(".button_action_edit");
+const popUpButton = document.querySelectorAll(".open-popup");
+const closeButton = document.querySelectorAll(".button_action_close");
+const submitButton = document.querySelectorAll(".form__button");
 
-const nameInput = formElement.querySelector(".form__item_el_name");
-const jobInput = formElement.querySelector(".form__item_el_job");
+const nameInput = document.querySelector(".form__item_el_name");
+const jobInput = document.querySelector(".form__item_el_job");
 
-function editProfile() {
-  formElement.classList.remove("form_hidden");
-
+function inputValueFiller() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+function openForm() {
+  popUpButton.forEach(function (el) {
+    el.addEventListener("click", function () {
+      if (el.classList.contains("button_action_edit")) {
+        formProfile.classList.remove("form_hidden");
+      }
+      else {
+        formPlace.classList.remove("form_hidden");
+      }
+      inputValueFiller();
+    })
+  })
+}
+
 function closeForm() {
-  formElement.classList.add("form_hidden");
+  closeButton.forEach(function(el) {
+    el.addEventListener("click", function () {
+      if (formProfile.classList.contains("form_hidden") === false) {
+        formProfile.classList.add("form_hidden");
+      }
+      else {
+        formPlace.classList.add("form_hidden");
+      }
+    })
+  })
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
+function formSubmitHandler() {
+  submitButton.forEach(function(el) {
+    el.addEventListener("click", function(evt) {
+      evt.preventDefault();
+      if (formProfile.classList.contains("form_hidden") === false) {
+        profileName.textContent = nameInput.value;
+        profileJob.textContent = jobInput.value;
 
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-
-  closeForm();
+        formProfile.classList.add("form_hidden");
+      }
+    })
+  })
 }
 
-editButton.addEventListener("click", editProfile);
-closeButton.addEventListener("click", closeForm);
-formElement.addEventListener("submit", formSubmitHandler);
+popUpButton.forEach(openForm);
+closeButton.forEach(closeForm);
+submitButton.forEach(formSubmitHandler);
