2f5b15c303cb146862e82abbd66df41d9504508c
Vladimir Chisty <v.chistyi@gmail.com>
2020-11-22 21:21:34 +0300

    refactor: rename script folder as scripts and create constants.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -96,11 +96,4 @@ const enableValidation = (object) => {
   });
 };
 
-enableValidation({
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__input-error_active"
-});
+enableValidation(validationConfig);
